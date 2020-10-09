da94390dd8a1be75811db982769dab4e012974bc
mariana.vns <mariana.vns@gmail.com>
2020-10-09 12:40:34 -0300

    Conclui a atividade 4 proposta nas instruções no README.md

--- src/controller/bookshopController.js
+++ src/controller/bookshopController.js
@@ -3,6 +3,8 @@ const collaborators = require( '../model/collaborators.json' )
 
 const fs = require('fs')
 
+/*  BOOKS  */
+
 const getAllBooks = ( req, res ) => {
     res.status(200).send(books)
 }
@@ -10,51 +12,67 @@ const getAllBooks = ( req, res ) => {
 const postBook = ( req, res ) => {
     const {id, isbn, name, authorship, publishingCompany, release, genre} = req.body
     books.push({id, isbn, name, authorship, publishingCompany, release, genre})
-    fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
-       if (err) {
-         return res.status(424).send({message: err});
-       }
-        console.log("O arquivo foi atualizado.")
-    })    
+        fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
+            if (err) {
+                return res.status(424).send({message: err});
+            }
+    console.log("O arquivo foi atualizado.")
+            })    
     res.status(200).send(books)
 }
 
 const deleteBook = ( req, res ) => {
-    const id = req.param.id
-    const filteredBook = books.filter((array) => array.id == id)
-    const indexFilteredBook = books.indexOf(filteredBook)
-    books.splice(indexFilteredBook)
-    fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
-        if (err) {
-        return res.status(424).send({message: err});
-        }
-        console.log("O arquivo foi atualizado.")
-    })
+    const id = req.params.id
+    const filteredBook = books.find((element) => element.id == id)
+    const index = books.indexOf(filteredBook)
+    books.splice(index, 1)
+        fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
+            if (err) {
+            return res.status(424).send({message: err});
+            }
+            console.log("O arquivo foi atualizado.")
+        })
 res.status(200).send(books)
 }
 
+        /* 
+        const getBookByGenre = ( req, res ) => {
+            res.status(200).send(booksByGenre)
+        } 
+        */
+
+
+/*  COLABORATORS  */
+       
 const getAllCollaborators = ( req, res ) => {
     res.status(200).send(collaborators)
 }
 
-// const postCollaborator = ( req, res ) => {
-//     res.status(200).send(collaboratorNewList)
-// }
+        /*
+        const postCollaborator = ( req, res ) => {
+            res.status(200).send(collaboratorNewList)
+        }
+        */
 
-// const deleteCollaborator = ( req, res ) => {
-//     res.status(200).send(collaborators)
-// }
+const deleteCollaborator = ( req, res ) => {
+    const id = req.params.id
+    const filtered = collaborators.find((element) => element.id == id)
+    const indexFiltered = collaborators.indexOf(filtered)
+    collaborators.splice(indexFiltered, 1)
+        fs.writeFile('./src/model/collaborators.json', JSON.stringify(collaborators), 'utf8', function(err){
+            if (err) {
+                return res.status(424).send({message: err});
+            }
+            console.log("O arquivo foi atualizado.")
+        })
+res.status(200).send(collaborators)
+}
 
-/////////////////
-
-// const getBookByGenre = ( req, res ) => {
-//     res.status(200).send(booksByGenre)
-// }
 
 const getAgeByID = ( req, res ) => {
     const actualYear = 2020
         const id = req.params.id
-        const filtered = collaborators.find((param) => param.id == id)
+        const filtered = collaborators.find((element) => element.id == id)
         const name = filtered.name
         const yearBirth = filtered.id.slice(4, -4)
     const age = actualYear - yearBirth
@@ -64,10 +82,10 @@ res.status(200).send(`A pessoa com o id ${id} se chama ${name} e tem ${age} anos
 module.exports = { 
     getAllBooks,
     postBook, 
-    deleteBook, 
+    deleteBook,
+    // getBookByGenre,
     getAllCollaborators,
     // postCollaborator,
-    // deleteCollaborator,
-    // getBookByGenre,
+    deleteCollaborator,
     getAgeByID
 }
--- src/router/bookshopRouter.js
+++ src/router/bookshopRouter.js
@@ -10,12 +10,12 @@ const router = express.Router()
 
 router.get('/livros', controller.getAllBooks)
 router.post('/adicionarlivro', controller.postBook)
-router.delete('/excluirlivro/:id', controller.deleteBook)
+router.delete('/excluirlivro/:id/', controller.deleteBook)
+// router.get('/livros/:genre', controller.getBookByGenre)
+
 router.get('/colaboradores', controller.getAllCollaborators)
 // router.post('/adicionarcolaborador'), controller.postCollaborator
-// router.delete('/colaborador/:id', controller.deleteCollaborator)
-
-// router.get('/livros/:genre', controller.getBookByGenre)
+router.delete('/excluircolaborador/:id', controller.deleteCollaborator)
 router.get('/idade/:id', controller.getAgeByID)
 
 module.exports = router
