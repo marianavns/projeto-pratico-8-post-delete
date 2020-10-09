b5fef30694fec9dae26aedc513f6a057c8bb4d8c
mariana.vns <mariana.vns@gmail.com>
2020-10-09 08:13:02 -0300

    Adiciona como rascunho a configuracao de seguranca no app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ app.use(express.json())
 
 app.use ('/', routes)
 
-module.exports = app  
+// As configuraçoes do projeto ficam aqui
+app.use(express.json())
 
+module.exports = app    
 
+// é uma garantia de segurança da aplicacao
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//     )
+//     next()
+//   })
+  
+>>>>>>> Stashed changes
