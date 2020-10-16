a1afa477f22c69bdd82adb2eb75555300d103be9
marianavns <mariana.vns@gmail.com>
2020-10-16 09:50:52 -0300

    Adiciona os métodos PUT e PATCH dos livros no controller

--- src/controller/bookshopController.js
+++ src/controller/bookshopController.js
@@ -1,25 +1,61 @@
+const fs = require('fs')
+
 const books = require( '../model/books.json' )
 const collaborators = require( '../model/collaborators.json' )
 
-const fs = require('fs')
-
-/*  BOOKS  */
+// ==> GET
 
 const getAllBooks = ( req, res ) => {
     res.status(200).send(books)
 }
 
+const getAllCollaborators = ( req, res ) => {
+    res.status(200).send(collaborators)
+}
+
+const getAgeByID = ( req, res ) => {
+        const id = req.params.id
+        const filtered = collaborators.find((element) => element.id == id)
+        const yearBirth = filtered.id.slice(4, -4)  
+            const name = filtered.name
+            const actualYear = 2020
+        const age = actualYear - yearBirth
+    res.status(200).send(`A pessoa com o id ${id} se chama ${name} e tem ${age} anos.`)
+}
+
+    /* 
+    const getBookByGenre = ( req, res ) => {
+        res.status(200).send(booksByGenre)
+    } 
+    */
+
+// ==> POST
+
 const postBook = ( req, res ) => {
     const {isbn, id, name, authorship, publishingCompany, release, genre} = req.body
-    books.push({id, isbn, id, name, authorship, publishingCompany, release, genre})
+    books.push({isbn, id, name, authorship, publishingCompany, release, genre})
         fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf8', function(err){
             if (err) {
-                return res.status(424).send({message: err});
+                return res.status(424).send({message: err})
             }
-    console.log("O arquivo foi atualizado.")
+    console.log("O arquivo foi atualizado!")
             })    
     res.status(201).send(books)
 }
+
+const postCollaborator = ( req, res ) => {
+    const {name, id, sector, occupation, workschedule, inTraining} = req.body
+    collaborators.push({name, id, sector, occupation, workschedule, inTraining})
+        fs.writeFile('./src/model/collaborators.json', JSON.stringify(collaborators), 'utf-8', function(err){
+            if (err) {
+                return res.status(424).send({message: err})
+            }
+    console.log("O arquivo foi atualizado!")
+            })    
+    res.status(201).send(collaborators)
+}
+
+// ==> DELETE
 
 const deleteBook = ( req, res ) => {
     const id = req.params.id
@@ -28,36 +64,11 @@ const deleteBook = ( req, res ) => {
     books.splice(index, 1)
         fs.writeFile('./src/model/books.json', JSON.stringify(books), 'utf-8', function(err){
             if (err) {
-            return res.status(424).send({message: err});
+            return res.status(424).send({message: err})
             }
-            console.log("O arquivo foi atualizado.")
+            console.log("O arquivo foi atualizado!")
         })
 res.status(200).send(books)
-}
-
-        /* 
-        const getBookByGenre = ( req, res ) => {
-            res.status(200).send(booksByGenre)
-        } 
-        */
-
-
-/*  COLABORATORS  */
-       
-const getAllCollaborators = ( req, res ) => {
-    res.status(200).send(collaborators)
-}
-
-const postCollaborator = ( req, res ) => {
-    const {name, id, sector, occupation, workschedule, inTraining} = req.body
-    collaborators.push({name, id, sector, occupation, workschedule, inTraining})
-        fs.writeFile('./src/model/collaborators.json', JSON.stringify(collaborators), 'utf-8', function(err){
-            if (err) {
-                return res.status(424).send({message: err});
-            }
-    console.log("O arquivo foi atualizado!")
-            })    
-    res.status(201).send(collaborators)
 }
 
 const deleteCollaborator = ( req, res ) => {
@@ -67,31 +78,83 @@ const deleteCollaborator = ( req, res ) => {
     collaborators.splice(indexFiltered, 1)
         fs.writeFile('./src/model/collaborators.json', JSON.stringify(collaborators), 'utf8', function(err){
             if (err) {
-                return res.status(424).send({message: err});
+                return res.status(424).send({message: err})
             }
             console.log("O arquivo foi atualizado.")
         })
 res.status(200).send(collaborators)
 }
 
+// ==> PUT
 
-const getAgeByID = ( req, res ) => {
-    const actualYear = 2020
-        const id = req.params.id
-        const filtered = collaborators.find((element) => element.id == id)
-        const name = filtered.name
-        const yearBirth = filtered.id.slice(4, -4)
-    const age = actualYear - yearBirth
-res.status(200).send(`A pessoa com o id ${id} se chama ${name} e tem ${age} anos.`)
+const putBook = (req, res) => {
+
+    const id = req.params.id
+    const bookUpdated = req.body
+        
+    try {
+        const bookToUpdate = books.find(element => element.id == id)
+        const index = books.indexOf(bookToUpdate)
+
+        books.splice(index, 1, bookUpdated)
+
+        fs.writeFile("./src/model/books.json", JSON.stringify(books), "utf-8", function (err) {
+            if (err) { return res.status(424).send( {message: 'err1'} ) }
+        console.log(`O arquivo com o ID ${id} foi atualizado com sucesso.`)
+        })
+
+    res.status(200).send(books)
+    }
+        catch (err) {
+            return res.status(424).send({ message: 'O arquivo não pôde ser processado.' })
+        }
 }
+
+// ==> PATCH
+
+const patchBook = (req, res) => {
+
+    const genre = req.params.genre
+    const updateBook = req.body
+    
+    try {
+        const bookToUpdate =  books.find(element => element.genre == genre)
+
+        Object.keys(updateBook).forEach(key => {
+            bookToUpdate[key] = updateBook[key]
+        })
+        console.log(bookToUpdate)
+
+        fs. writeFile("./src/model/books.json", JSON.stringify(books), "utf8", function(err){
+            if (err) {
+                return res.status(424).send({ message: "err1" }) }
+        console.log(`Os registros que contem ${genre} foram atualizados.`)
+        })
+    
+    return res.status(200).send(books)
+    } catch (err) {
+        return res.status(424).send({ message: `Não foi possível atualizar a chave` })
+      }
+}
+
+
+
 
 module.exports = { 
     getAllBooks,
-    postBook, 
-    deleteBook,
-    // getBookByGenre,
     getAllCollaborators,
+    getAgeByID,
+    // getBookByGenre,
+
+    postBook,
     postCollaborator,
+
+    deleteBook,  
     deleteCollaborator,
-    getAgeByID
+
+    putBook,
+    // putCollaborator,
+
+    patchBook,
+    // patchBook
 }
